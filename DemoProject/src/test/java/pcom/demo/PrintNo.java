ce9232b3e360c9129f130effbf22c97de8e67f30
devbisen95 <Focus@host.docker.internal>
2020-12-22 22:27:21 +0530

    Added DemoProjectCode

---
+++ DemoProject/src/test/java/pcom/demo/PrintNo.java
@@ -0,0 +1,13 @@
+package pcom.demo;
+
+public class PrintNo {
+
+	public static void main(String[] args) {
+		  for(int i = 1; i <= 10; i++)
+	        {
+	            System.out.println(i);
+	        }
+
+	}
+
+}
